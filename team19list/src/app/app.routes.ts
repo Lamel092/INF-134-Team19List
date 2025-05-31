8f808e639189b93793a8dd318bc508f236285817
Matthew <matthew.duong888@gmail.com>
2025-05-30 20:17:54 -0700

    category page

--- team19list/src/app/app.routes.ts
+++ team19list/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CategoryPageComponent } from './category-page/category-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'category-page', component: CategoryPageComponent },
+];
